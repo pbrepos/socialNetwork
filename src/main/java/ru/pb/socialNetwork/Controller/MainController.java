311cb2e8193038301df7d1013f08f388fdb35af8
pb <pbarmenkov@yandex.ru>
2021-01-15 19:53:33 +0300

    [less-4] Spring Boot Security: К сообщению добавили автора

--- src/main/java/ru/pb/socialNetwork/Controller/MainController.java
+++ src/main/java/ru/pb/socialNetwork/Controller/MainController.java
@@ -1,11 +1,13 @@
 package ru.pb.socialNetwork.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.pb.socialNetwork.domain.Message;
+import ru.pb.socialNetwork.domain.User;
 import ru.pb.socialNetwork.repo.MessageRepo;
 
 import java.util.Map;
@@ -29,8 +31,10 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String addMessage(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String addMessage(@AuthenticationPrincipal User user,
+                             @RequestParam String text,
+                             @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
