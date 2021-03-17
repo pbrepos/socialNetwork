f394f5d2615c4b84faa68a1fc2ee47453bf71eff
pb <pbarmenkov@yandex.ru>
2021-03-17 17:40:19 +0300

    [less-10] Spring Boot FlyWay: миграции БД, профиль пользователя

--- src/main/java/ru/pb/socialNetwork/service/UserService.java
+++ src/main/java/ru/pb/socialNetwork/service/UserService.java
@@ -10,9 +10,8 @@ import ru.pb.socialNetwork.domain.Role;
 import ru.pb.socialNetwork.domain.User;
 import ru.pb.socialNetwork.repo.UserRepo;
 
-import java.util.Collections;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -45,6 +44,12 @@ public class UserService implements UserDetailsService {
 
         userRepo.save(user);
 
+        sendEmailMessage(user);
+
+        return true;
+    }
+
+    private void sendEmailMessage(User user) {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s \n" +
@@ -54,15 +59,13 @@ public class UserService implements UserDetailsService {
             );
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
         User user = userRepo.findByActivationCode(code);
 
         if (Objects.isNull(user)) {
-            return  false;
+            return false;
         }
 
         user.setActivationCode(null);
@@ -70,6 +73,49 @@ public class UserService implements UserDetailsService {
         userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (Objects.nonNull(email) && !email.equals(userEmail)) || (Objects.nonNull(userEmail) && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+
+        }
+
+        userRepo.save(user);
+
+        if (isEmailChanged) {
+            sendEmailMessage(user);
+        }
     }
 }
 
---
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -0,0 +1,3 @@
+INSERT into usr (id, username, password, is_active) values (1, 'admin', 'admin', true);
+
+insert into user_role (user_id, roles) values (1, 'USER'), (1, 'ADMIN');
